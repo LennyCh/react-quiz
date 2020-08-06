e3469cef7b6a1160adae8f0cfdf1b5c4a20b93a9
LennyCh <lena.chepkasova.95@mail.ru>
2020-08-06 10:40:26 +0300

    click handling

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import classes from './ActiveQuiz.css';
+import AnswersList from './AnswersList/AnswersList';
 
-const ActiveQuiz = props => (
-    <div className={classes.ActiveQuiz}>
-        <p className={classes.Question}>
+const ActiveQuiz = props => {
+    return (
+        <div className={classes.ActiveQuiz}>
+            <p className={classes.Question}>
             <span>
                 <strong>1.</strong>&nbsp;
-                Вопрос?
+                {props.question}
             </span>
-            <small>4 из 12</small>
-        </p>
+                <small>4 из 12</small>
+            </p>
 
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
-    </div>
-);
+            <AnswersList
+                answers={props.answers}
+                onAnswerClick={props.onAnswerClick}
+            />
+        </div>
+    );
+};
 
 export default ActiveQuiz;
