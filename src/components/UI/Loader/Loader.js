97ec8031560b99d61f407e5ed59e4e2b14dbfe7d
LennyCh <lena.chepkasova.95@mail.ru>
2020-12-24 20:20:59 +0300

    authorization and quiz creation (with backend)

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './Loader.css';
+
+const Loader = props => (
+    <div className={classes.LoaderContainer}>
+        <div className={classes.Loader}>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+            <div>
+                <div/>
+            </div>
+        </div>
+    </div>
+
+);
+
+export default Loader;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import classes from './Auth.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
+import axios from '../../axios/axios-quiz';
 import is from 'is_js';
 
 export default class Auth extends Component {
 
     state = {
         isFormValid: false,
+        successRegister: false,
         formControls: {
             email: {
                 value: '',
@@ -25,7 +27,7 @@ export default class Auth extends Component {
                 value: '',
                 type: 'password',
                 label: 'Пароль',
-                errorMessage: 'Введите корректный пароль',
+                errorMessage: 'Введите хотя бы 6 символов',
                 valid: false,
                 touched: false,
                 validation: {
@@ -36,12 +38,35 @@ export default class Auth extends Component {
         }
     };
 
-    loginHandler = () => {
-
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBE78w5dxtUJLXY6jE8OTNl8EvliQDMGCs', authData);
+            console.log(response.data);
+        } catch (e) {
+            console.log(e);
+        }
     };
 
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBE78w5dxtUJLXY6jE8OTNl8EvliQDMGCs', authData);
+            console.log(response);
+            this.setState({
+                successRegister: true,
+            });
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     submitHandler = e => {
@@ -136,6 +161,12 @@ export default class Auth extends Component {
                         >
                             Зарегистрироваться
                         </Button>
+
+                        {
+                            this.state.successRegister
+                                ? <h4>Вы успешно зарегистровались!</h4>
+                                : null
+                        }
                     </form>
                 </div>
             </div>
